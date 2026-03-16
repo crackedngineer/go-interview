d9462560c587b70d40b88cf23365ca079d69c623
Subhomoy Roy Choudhury <subhomoyrchoudhury@gmail.com>
2026-03-16 11:19:16 +0530

    Refactor main.go to implement HTTP server with graceful shutdown and restore local.yaml configuration

--- golang/crud_api/cmd/student_api/main.go
+++ golang/crud_api/cmd/student_api/main.go
@@ -1,7 +1,57 @@
 package main
 
-import "fmt"
+import (
+	"context"
+	"fmt"
+	"log"
+	"log/slog"
+	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
+
+	"github.com/crackedngineer/go-interview/internal/config"
+)
 
 func main() {
-	fmt.Println("Hello world")
+	// load Config
+	cfg := config.MustLoadConfig()
+	// DB setup
+
+	// setup router
+	router := http.NewServeMux()
+	router.HandleFunc("GET /", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("Hello World"))
+	})
+	// setup server
+	server := http.Server{
+		Addr:    cfg.HttpServer.Addr,
+		Handler: router,
+	}
+	fmt.Println("Server started at", cfg.HttpServer.Addr)
+
+	// graceful shutdown
+	done := make(chan os.Signal, 1)
+
+	signal.Notify(done, os.Interrupt, syscall.SIGINT, syscall.SIGTERM)
+
+	go func() {
+		err := server.ListenAndServe()
+		if err != nil {
+			log.Fatalf("failed to start server: %v", err)
+		}
+	}()
+
+	<-done
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	err := server.Shutdown(ctx)
+	if err != nil {
+		slog.Error("failed to shutdown server", slog.String("error", err.Error()))
+	}
+
+	slog.Info("shutting down server")
+
 }
